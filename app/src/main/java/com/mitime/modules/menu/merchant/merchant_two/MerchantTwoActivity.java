2a5344af4cde7079f962259559e547382e313781
yh <2366081200@qq.com>
2017-12-12 10:32:09 +0800

    YH 商家2级页面雏形
    Signed-off-by: yh <“2366081200@qq.com>

---
+++ app/src/main/java/com/mitime/modules/menu/merchant/merchant_two/MerchantTwoActivity.java
@@ -0,0 +1,18 @@
+package com.mitime.modules.menu.merchant.merchant_two;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mitime.R;
+
+/**
+ * 商家2级分类
+ */
+public class MerchantTwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_merchant_two);
+    }
+}
